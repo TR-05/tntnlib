4b5526fac1e8ca3379d2d537f78f9d1846a6b31b
Trevor <63212388+TR-05@users.noreply.github.com>
2023-11-22 11:27:51 -0800

    structs suck

--- src/autos.cpp
+++ src/autos.cpp
@@ -1,13 +1,14 @@
 #include "autos.h"
 #include "vex.h"
-using namespace vex;
+using namespace tntnlib;
 
 void programming_skills()
 {
   printf("Entered Auto\n");
   chassis.initialize(false, 0,0,0);
   chassis.stateMachineOn();
-  float akp = angularController.kP;
+
+  /*float akp = angularController.kP;
   float aki = angularController.kI;
   float akd = angularController.kD;
   float lkp = linearController.kP;
@@ -16,15 +17,15 @@ void programming_skills()
 
   //chassis.tuneOffsets(3600, akp, aki, akd, 6, 2); //Tunes odom + imu constants: DISABLE TERMINAL PRINT then jig bot to a tile, run auto, once bot stops moving tap and hold brain screen
   chassis.moveTo(30, 30, false, 12, 12, lkp, lki, lkd, akp, aki, akd*1.25, .3, 2);
-  wait(200, msec);
+  vex::wait(200, vex::msec);
   chassis.moveTo(0, 60, true, 12, 12, lkp, lki, lkd, akp, aki, akd*1.25, .3, 2);
-  wait(200, msec);
+  vex::wait(200, vex::msec);
 
   chassis.boomerangTo(0, 20, -90, false, 9, 12, lkp, lki, lkd, akp, aki, akd, .3, .5, 2);
-  wait(200, msec);
+  vex::wait(200, vex::msec);
   chassis.boomerangTo(20, 20, 90, false, 9, 12, lkp, lki, lkd, akp, aki, akd, .3, .5, 2);
-  wait(200, msec);
+  vex::wait(200, vex::msec);
   chassis.boomerangTo(0, 0, 0, false, 9, 12, lkp, lki, lkd, akp, aki, akd, .3, .5, 2);
-  wait(200, msec);
-  chassis.turnToHeading(0, false, 9, akp, aki, akd, 1);
+  vex::wait(200, vex::msec);
+  chassis.turnToHeading(0, false, 9, akp, aki, akd, 1);*/
 }
--- tntnlibrary/include/chassis/structs.h
+++ tntnlibrary/include/chassis/structs.h
@@ -4,75 +4,84 @@
 #include "../tntnlibrary/include/devices/trackingWheel.h"
 #include "../tntnlibrary/include/devices/gyro.h"
 
-namespace tntnlib {
-/**
- * @brief Struct containing all the sensors used for odometry
- *
- * The sensors are stored in a struct so that they can be easily passed to the chassis class
- * The variables are pointers so that they can be set to nullptr if they are not used
- * Otherwise the chassis class would have to have a constructor for each possible combination of sensors
- *
- * @param vertical1 pointer to the first vertical tracking wheel
- * @param vertical2 pointer to the second vertical tracking wheel
- * @param horizontal1 pointer to the first horizontal tracking wheel
- * @param horizontal2 pointer to the second horizontal tracking wheel
- * @param imu pointer to the IMU
- */
-typedef struct {
-        TrackingWheel* vertical1;
-        TrackingWheel* vertical2;
-        TrackingWheel* horizontal1;
-        TrackingWheel* horizontal2;
-        Gyro* imu;
-} OdomSensors_t;
+namespace tntnlib
+{
+        /**
+         * @brief Struct containing all the sensors used for odometry
+         *
+         * The sensors are stored in a struct so that they can be easily passed to the chassis class
+         * The variables are pointers so that they can be set to nullptr if they are not used
+         * Otherwise the chassis class would have to have a constructor for each possible combination of sensors
+         *
+         * @param vertical1 pointer to the first vertical tracking wheel
+         * @param vertical2 pointer to the second vertical tracking wheel
+         * @param horizontal1 pointer to the first horizontal tracking wheel
+         * @param horizontal2 pointer to the second horizontal tracking wheel
+         * @param imu pointer to the IMU
+         */
+        typedef struct
+        {
+                TrackingWheel *vertical1;
+                TrackingWheel *vertical2;
+                TrackingWheel *horizontal1;
+                TrackingWheel *horizontal2;
+                Gyro *imu;
+        } OdomSensors_t;
 
-/**
- * @brief Struct containing constants for a chassis controller
- *
- * The constants are stored in a struct so that they can be easily passed to the chassis class
- * Set a constant to 0 and it will be ignored
- *
- * @param kP proportional constant for the chassis controller
- * @param kD derivative constant for the chassis controller
- * @param smallError the error at which the chassis controller will switch to a slower control loop
- * @param smallErrorTimeout the time the chassis controller will wait before switching to a slower control loop
- * @param largeError the error at which the chassis controller will switch to a faster control loop
- * @param largeErrorTimeout the time the chassis controller will wait before switching to a faster control loop
- * @param slew the maximum acceleration of the chassis controller
- */
-typedef struct {
-        float kP;
-        float kI;
-        float kD;
-        float kIStart;
-        float kIMax;
-        float smallError;
-        float smallErrorTimeout;
-        float largeError;
-        float largeErrorTimeout;
-        float slew;
-} ChassisController_t;
+        /**
+         * @brief Struct containing constants for a chassis controller
+         *
+         * The constants are stored in a struct so that they can be easily passed to the chassis class
+         * Set a constant to 0 and it will be ignored
+         *
+         * @param kP proportional constant for the chassis controller
+         * @param kD derivative constant for the chassis controller
+         * @param smallError the error at which the chassis controller will switch to a slower control loop
+         * @param smallErrorTimeout the time the chassis controller will wait before switching to a slower control loop
+         * @param largeError the error at which the chassis controller will switch to a faster control loop
+         * @param largeErrorTimeout the time the chassis controller will wait before switching to a faster control loop
+         * @param slew the maximum acceleration of the chassis controller
+         */
+        typedef struct
+        {
+                float kP;
+                float kI;
+                float kD;
+                float kIStart;
+                float kIMax;
+                float smallError;
+                float smallErrorTimeout;
+                float largeError;
+                float largeErrorTimeout;
+                float slew;
+        } ChassisController_t;
 
-/**
- * @brief Struct containing constants for a drivetrain
- *
- * The constants are stored in a struct so that they can be easily passed to the chassis class
- * Set a constant to 0 and it will be ignored
- *
- * @param leftMotors pointer to the left motors
- * @param rightMotors pointer to the right motors
- * @param trackWidth the track width of the robot
- * @param wheelDiameter the diameter of the wheel used on the drivetrain
- * @param rpm the rpm of the wheels
- * @param chasePower higher values make the robot move faster but causes more overshoot on turns
- */
-typedef struct {
-        vex::motor_group* leftMotors;
-        vex::motor_group* rightMotors;
-        float trackWidth;
-        float wheelDiameter;
-        float cartridgeRPM;
-        float rpm;
-        float chasePower;
-} Drivetrain_t;
+        /**
+         * @brief Struct containing constants for a drivetrain
+         *
+         * The constants are stored in a struct so that they can be easily passed to the chassis class
+         * Set a constant to 0 and it will be ignored
+         *
+         * @param leftMotors pointer to the left motors
+         * @param rightMotors pointer to the right motors
+         * @param trackWidth the track width of the robot
+         * @param wheelDiameter the diameter of the wheel used on the drivetrain
+         * @param rpm the rpm of the wheels
+         * @param chasePower higher values make the robot move faster but causes more overshoot on turns
+         */
+        typedef struct
+        {
+                vex::motor_group *leftMotors;
+                vex::motor_group *rightMotors;
+                float trackWidth;
+                float wheelDiameter;
+                float cartridgeRPM;
+                float rpm;
+                float chasePower;
+        } Drivetrain_t;
+        //extern tntnlib::OdomSensors_t sensors;
+        //extern ChassisController_t linearController;
+        //extern ChassisController_t angularController;
+        //extern tntnlib::Drivetrain_t drivebase;
+
 } // namespace tntnlib
