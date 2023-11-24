6b2207f65428bd6481969422b7c002bf4b92fc0b
Trevor <63212388+TR-05@users.noreply.github.com>
2023-11-23 18:02:07 -0800

    added 24 config

--- 24/src/main.cpp
+++ 24/src/main.cpp
@@ -9,25 +9,39 @@ brain Brain;
 /* tntnlib robot Config */
 
 /* drive motors */
-motor ls_front = motor(8 - 1, ratio6_1, true);
-motor ls_back = motor(10 - 1, ratio6_1, true);
-motor rs_front = motor(PORT18, ratio6_1, false);
-motor rs_back = motor(PORT19, ratio6_1, false);
-motor_group leftMotors = motor_group(ls_front, ls_back);
-motor_group rightMotors = motor_group(rs_front, rs_back);
+motor ls_front = motor(6 - 1, ratio6_1, true);
+motor ls_mid = motor(7 - 1, ratio6_1, true);
+motor ls_back = motor(8 - 1, ratio6_1, true);
+motor rs_front = motor(3 - 1, ratio6_1, false);
+motor rs_mid = motor(13 - 1, ratio6_1, false);
+motor rs_back = motor(14 - 1, ratio6_1, false);
+motor_group leftMotors = motor_group(ls_front, ls_mid, ls_back);
+motor_group rightMotors = motor_group(rs_front, rs_mid, rs_back);
 
 /* tracking wheels and gyro */
-tntnlib::TrackingWheel horizontal(Brain.ThreeWirePort.G, tntnlib::Omniwheel::NEW_275, 0.002292, 1);
-//tntnlib::TrackingWheel vertical(Brain.ThreeWirePort.E, tntnlib::Omniwheel::NEW_275, -0.253611, 1);
-tntnlib::TrackingWheel vertical(&leftMotors, tntnlib::Omniwheel::NEW_275, -0.253611, 600, 360);
+// tntnlib::TrackingWheel horizontal(Brain.ThreeWirePort.G, tntnlib::Omniwheel::NEW_275, 0.002292, 1);
+// tntnlib::TrackingWheel vertical(Brain.ThreeWirePort.E, tntnlib::Omniwheel::NEW_275, -0.253611, 1);
+tntnlib::TrackingWheel vertical(&leftMotors, tntnlib::Omniwheel::NEW_4, -8, 600, 300);
 tntnlib::Gyro imu(1, 1.010357);
 
 /* chassis and controllers */
 tntnlib::ControllerSettings tntnlib::linearSettings(.6, 0, 3.5, 0, 0, 12);
 tntnlib::ControllerSettings tntnlib::angularSettings(.25, 0.01, 2.0, 10, 2, 12);
 tntnlib::Drivetrain tntnlib::drivetrain(&leftMotors, &rightMotors, 10.0, tntnlib::Omniwheel::OLD_325, 360, 8);
-tntnlib::OdomSensors tntnlib::sensors(&vertical, nullptr, &horizontal, nullptr, &imu);
+tntnlib::OdomSensors tntnlib::sensors(&vertical, nullptr, nullptr, nullptr, &imu);
 /* End of tntnlib Robot Config */
+
+motor DFWl1 = motor(PORT11, ratio6_1, true);
+motor DFWl2 = motor(PORT12, ratio6_1, false);
+motor DFWr1 = motor(PORT20, ratio6_1, false);
+motor DFWr2 = motor(PORT19, ratio6_1, true);
+
+motor left_intake = motor(PORT10, ratio6_1, false);
+motor right_intake = motor(PORT17, ratio6_1, true);
+
+digital_out left_intake_piston = digital_out(Brain.ThreeWirePort.A);
+digital_out right_intake_piston = digital_out(Brain.ThreeWirePort.B);
+motor_group DFW = motor_group(DFWl1, DFWl2, DFWr1, DFWr2);
 
 /* data logger idk where to put :/ */
 int logger()
@@ -64,8 +78,48 @@ void usercontrol()
   printf("Entered Driver\n");
   chassis.stateMachineOff();
 
+  bool flywheelOn = false;
+  // User control code here, inside the loop
+  float volts = 0;
+  bool pistonState = false;
   while (1)
   {
+
+    if (Controller.ButtonA.pressing())
+    {
+      volts = 12;
+      flywheelOn = true;
+    }
+
+    if (Controller.ButtonB.pressing())
+    {
+      volts = 9.5;
+      flywheelOn = true;
+    }
+    if (Controller.ButtonL2.pressing())
+    {
+      flywheelOn = false;
+    }
+
+    if (flywheelOn)
+    {
+      DFW.spin(fwd, volts, volt);
+    }
+    else
+    {
+      DFW.stop(coast);
+    }
+    if (Controller.ButtonL1.pressing() && !pistonState)
+    {
+      left_intake_piston.set(!left_intake_piston);
+      right_intake_piston.set(!left_intake_piston);
+    }
+    pistonState = Controller.ButtonL1.pressing();
+
+    float intakePower = 12 * (Controller.ButtonR1.pressing() - Controller.ButtonR2.pressing());
+    left_intake.spin(fwd, intakePower, volt);
+    right_intake.spin(fwd, intakePower, volt);
+
     chassis.tank(Controller.Axis3.position(), Controller.Axis2.position(), 100); // tank (the best drive style)
     // chassis.arcade(Controller.Axis3.position(), Controller.Axis4.position(), 0); //single stick arcade
     // chassis.arcade(Controller.Axis3.position(), Controller.Axis1.position(), 0); //split arcade
