348bd0a2d473434bb90754a0701538405aa4b0e0
Trevor <trevor.ruttan05@gmail.com>
2024-02-15 19:39:22 -0800

    updated 15 controls

--- glove/src/main.cpp
+++ glove/src/main.cpp
@@ -1,7 +1,8 @@
-#include "vex.h"
+#include <iostream>
+
 #include "../tntnlibrary/include/api.h"
 #include "autos.h"
-#include <iostream>
+#include "vex.h"
 using namespace tntnlib;
 vex::competition Competition;
 vex::brain Brain;
@@ -29,106 +30,105 @@ vex::digital_out left_wing(Brain.ThreeWirePort.F);
 vex::digital_out right_wing(Brain.ThreeWirePort.E);
 vex::digital_out hang(Brain.ThreeWirePort.G);
 
-/* data logger idk where to put :/ */
 int logger()
 {
-  while (true)
-  {
-    Pose current(chassis.getPose(false));
-    // printf("SX: %.2f, SR: %.2f, IMU: %.2f ", chassis.sensors.horizontal1 != nullptr ? chassis.sensors.horizontal1->getDistance() : 0, chassis.sensors.vertical1 != nullptr ? chassis.sensors.vertical1->getDistance() : 0, chassis.sensors.gyro != nullptr ? chassis.sensors.gyro->rotation() : 0);
-    printf("  X: %.2f,  Y: %.2f,  H: %.2f   T: %.2f ET:%.2f\n", current.x, current.y, current.theta, getTime(), totalTime / 1000.0);
-    Brain.Screen.clearScreen(vex::color(255, 75, 0));
-    Brain.Screen.print("X:%6.2f, Y:%6.2f, H:%6.2f", current.x, current.y, current.theta);
-    vex::wait(50, vex::msec);
-  }
-  return 0;
+    while (true)
+    {
+        Pose current(chassis.getPose(false));
+        // printf("SX: %.2f, SR: %.2f, IMU: %.2f ", chassis.sensors.horizontal1 != nullptr ? chassis.sensors.horizontal1->getDistance() : 0, chassis.sensors.vertical1 != nullptr ? chassis.sensors.vertical1->getDistance() : 0, chassis.sensors.gyro != nullptr ? chassis.sensors.gyro->rotation() : 0);
+        printf("  X: %.2f,  Y: %.2f,  H: %.2f   T: %.2f ET:%.2f\n", current.x, current.y, current.theta, getTime(), totalTime / 1000.0);
+        Brain.Screen.clearScreen(vex::color(255, 75, 0));
+        Brain.Screen.print("X:%6.2f, Y:%6.2f, H:%6.2f", current.x, current.y, current.theta);
+        vex::wait(50, vex::msec);
+    }
+    return 0;
 }
-
 
 int chassisLoop()
 {
-  while (true)
-  {
-    chassis.update();
-    vex::wait(10, vex::msec);
-  }
-  return 0;
+    while (true)
+    {
+        chassis.update();
+        vex::wait(10, vex::msec);
+    }
+    return 0;
 }
 
 void resetThreads()
 {
-  loggerThread.interrupt();
-  loggerThread = vex::thread(logger);
-  chassisThread.interrupt();
-  chassisThread = vex::thread(chassisLoop);
+    loggerThread.interrupt();
+    loggerThread = vex::thread(logger);
+    chassisThread.interrupt();
+    chassisThread = vex::thread(chassisLoop);
 }
 
 /* runs when program first starts */
 void pre_auton()
 {
-  printf("Entered pre_auton\n");
-  chassis.initialize(true, 0, 0, 0);
-  resetThreads();
+    printf("Entered pre_auton\n");
+    chassis.initialize(true, 0, 0, 0);
+    resetThreads();
 }
 
 /* runs on comp switch autonomous */
 void autonomous()
 {
-  resetThreads();
-  //safeAWP();
-  //safeElim();
-  awp();
-  //programming_skills();
-  //ElimAwp();
+    resetThreads();
+    // safeAWP();
+    // safeElim();
+    awp();
+    // programming_skills();
+    // ElimAwp();
 }
 
 /* runs on comp switch driver */
 void usercontrol()
 {
-  resetThreads();
-  printf("Entered Driver\n");
-  chassis.stateMachineOff();
-  if (Controller.ButtonLeft.pressing())
-  {
-    vex::wait(2600, vex::msec);
-    autonomous();
-  }
-
-  while (1)
-  {
-    updateButtons();
-    if (l1.newPress())
+    resetThreads();
+    printf("Entered Driver\n");
+    chassis.stateMachineOff();
+    if (Controller.ButtonLeft.pressing())
     {
-      left_wing.set(!left_wing.value());
-      right_wing.set(!left_wing.value());
+        vex::wait(2600, vex::msec);
+        autonomous();
     }
-    if (y.newPress())
-      left_wing.set(!left_wing.value());
-    if (a.newPress())
-      right_wing.set(!right_wing.value());
-    if (b.newPress())
-      hang.set(1);
-    if (x.newPress())
-      hang.set(0);
 
-    intake.driverTwoButton(Controller.ButtonR1.pressing(), Controller.ButtonR2.pressing(), 12, -12);
-    intake.setBrakeType(vex::brakeType::brake);
-    // chassis.tank(Controller.Axis3.position() *.12, Controller.Axis2.position() *.12, 100); // tank
-    chassis.arcade(Controller.Axis3.position() * .12, Controller.Axis4.position() * .12, 0); // single stick arcade
-    // chassis.arcade(Controller.Axis3.position() *.12, Controller.Axis1.position() *.12, 0); //split arcade
-    vex::wait(10.0, vex::msec);
-  }
+    while (1)
+    {
+        updateButtons();
+        if (l1.state or a.state)
+            left_wing.set(1);
+        else
+            left_wing.set(0);
+
+        if (l1.state or y.state)
+            right_wing.set(1);
+        else
+            right_wing.set(0);
+
+        if (b.state or x.state)
+            hang.set(1);
+        else
+            hang.set(0);
+
+        intake.driverTwoButton(Controller.ButtonR1.pressing(), Controller.ButtonR2.pressing(), 12, -12);
+        intake.setBrakeType(vex::brakeType::brake);
+        // chassis.tank(Controller.Axis3.position() *.12, Controller.Axis2.position() *.12, 100); // tank
+        chassis.arcade(Controller.Axis3.position() * .12, Controller.Axis4.position() * .12, 0);  // single stick arcade
+        // chassis.arcade(Controller.Axis3.position() *.12, Controller.Axis1.position() *.12, 0); //split arcade
+        vex::wait(10.0, vex::msec);
+    }
 }
 
 /* comp switch callbacks */
 int main()
 {
-  Competition.autonomous(autonomous);
-  Competition.drivercontrol(usercontrol);
-  pre_auton();
+    Competition.autonomous(autonomous);
+    Competition.drivercontrol(usercontrol);
+    pre_auton();
 
-  while (true)
-  {
-    vex::wait(100, vex::msec);
-  }
+    while (true)
+    {
+        vex::wait(100, vex::msec);
+    }
 }
